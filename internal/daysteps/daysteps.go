c770f90e0ad24be515dc72e202883e8ea6e991d7
Alexey Lampsi <zakcore1@yandex.ru>
2025-03-04 18:53:23 +0300

    second commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,12 @@
 package daysteps
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/exercise/internal/spentcalories"
 )
 
 var (
@@ -9,7 +14,28 @@ var (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// ваш код ниже
+	var steps int
+	var duration time.Duration
+	var err error
+
+	partsData := strings.Split(data, ",")
+	if len(partsData) != 2 {
+		return 0, 0, fmt.Errorf("error partsData need 2 parts, got %d", len(partsData))
+	}
+
+	stepsStr := strings.TrimSpace(partsData[0])
+	durationStr := strings.TrimSpace(partsData[1])
+
+	steps, err = strconv.Atoi(stepsStr)
+	if err != nil {
+		return 0, 0, fmt.Errorf("error convert stepStr to integer")
+	}
+
+	duration, err = time.ParseDuration(durationStr)
+	if err != nil {
+		return 0, 0, fmt.Errorf("error convert durationStr to time.Duration")
+	}
+	return steps, duration, nil
 }
 
 // DayActionInfo обрабатывает входящий пакет, который передаётся в
@@ -19,5 +45,18 @@ func parsePackage(data string) (int, time.Duration, error) {
 // Если пакет валидный, он добавляется в слайс storage, который возвращает
 // функция. Если пакет невалидный, storage возвращается без изменений.
 func DayActionInfo(data string, weight, height float64) string {
-	// ваш код ниже
+	steps, duration, err := parsePackage(data)
+	if err != nil {
+		fmt.Errorf("error DayActionInfo, failed parsePackage(data)")
+		return ""
+	}
+
+	if steps <= 0 {
+		return ""
+	}
+	distance := float64(steps) * StepLength
+	kmDistance := distance / 10
+	kalories := spentcalories.WalkingSpentCalories(steps, weight, height, duration) // (steps int, weight, height float64, duration time.Duration) float64
+
+	return fmt.Sprintf(" Колличество шагов: %d\n Дистанция составила %.2fкм.\n Вы сожгли %.2f ккал.", steps, kmDistance, kalories)
 }
